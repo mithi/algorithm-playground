9183e9242c2665bef4b0921bf5a35c3272fcf7c2
Mithi Sevilla <mithi.sevilla@gmail.com>
2018-06-25 18:12:02 +0800

    style: Refactor main

--- rust-algo/unionfind/src/main.rs
+++ rust-algo/unionfind/src/main.rs
@@ -1,50 +1,39 @@
 extern crate unionfind;
 
+use std::fmt::Debug;
 use unionfind::{UnionFind, QuickFind, QuickUnion};
+
+fn check_uf<T: UnionFind + Debug>(q: &mut T) {
+
+    println!("{:?}", q);
+
+    let i = q.connected(4, 7);
+    let j = q.connected(3, 6);
+    let k = q.connected(1, 2);
+
+    println!("{} {} {}", i, j, k);
+
+    q.union(4, 3);
+    println!("{:?} ", q); 
+    
+    q.union(3, 8);
+    println!("{:?} ", q);
+
+    let i = q.connected(4, 3);
+    let j = q.connected(8, 3);
+    let k = q.connected(4, 8);
+
+    println!("{} {} {}", i, j, k);
+}
+
 
 fn main() {
 
     println!("QuickFind!");
     let mut q = QuickFind::new(10);
-    println!("{:?}", q);
-
-    let i = q.connected(4, 7);
-    let j = q.connected(3, 6);
-    let k = q.connected(1, 2);
-
-    println!("{} {} {}", i, j, k);
-
-    q.union(4, 3);
-    println!("{:?} ", q); // [0, 1, 2, 3, 3, 5, 6, 7, 8, 9]
-    q.union(3, 8);
-    println!("{:?} ", q); // [0, 1, 2, 8, 8, 5, 6, 7, 8, 9]
-
-    let i = q.connected(4, 3);
-    let j = q.connected(8, 3);
-    let k = q.connected(4, 8);
-
-    println!("{} {} {}", i, j, k);
-
-// -------------------------------------------------------------
+    check_uf(&mut q);
 
     println!("QuickUnion!");
     let mut q = QuickUnion::new(10);
-    println!("{:?}", q);
-
-    let i = q.connected(4, 7);
-    let j = q.connected(3, 6);
-    let k = q.connected(1, 2);
-
-    println!("{} {} {}", i, j, k);
-
-    q.union(4, 3);
-    println!("{:?} ", q); // [0, 1, 2, 3, 3, 5, 6, 7, 8, 9]
-    q.union(3, 8);
-    println!("{:?} ", q); // [0, 1, 2, 8, 3, 5, 6, 7, 8, 9]
-
-    let i = q.connected(4, 3);
-    let j = q.connected(8, 3);
-    let k = q.connected(4, 8);
-
-    println!("{} {} {}", i, j, k);
+    check_uf(&mut q);
 }
