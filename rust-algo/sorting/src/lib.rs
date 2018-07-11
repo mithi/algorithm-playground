f511bd22e58aaf9042a5427463818eae4c5cd0ef
Mithi Sevilla <mithi.sevilla@gmail.com>
2018-07-11 21:55:18 +0800

    feat: Add shellshort

--- rust-algo/sorting/src/lib.rs
+++ rust-algo/sorting/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod selection {
 
     pub fn sort<T: PartialOrd>(v: &mut [T]) {
-        
+
         for i in 0..v.len() {
             let j = get_min(i, v);
             v.swap(j, i);
@@ -13,11 +13,11 @@ pub mod selection {
         let mut min_index = i;
         let mut current_min = &v[i];
 
-        for j in i..v.len() {    
+        for j in i..v.len() {
 
-            if v[j] < *current_min { 
+            if v[j] < *current_min {
                 current_min = &v[j];
-                min_index = j; 
+                min_index = j;
             }
         }
         min_index
@@ -31,9 +31,51 @@ pub mod insertion {
 
         for i in 0..v.len() {
             for j in (1..=i).rev() {
-                if v[j] >= v[j - 1] { break; }               
+                if v[j] >= v[j - 1] { break; }
                 v.swap(j, j - 1);
-            } 
+            }
         }
     }
 }
+
+
+pub mod shell {
+
+    pub fn sort<T: PartialOrd>(array: &mut [T], gaps: &[usize]) {
+        for gap in gaps {
+
+            if array.len() < *gap {
+                continue;
+            }
+
+            for i in 0..*gap {
+
+                let mut index_pointer = i;
+
+                while index_pointer < array.len() {
+                    let min_index = get_min(index_pointer, *gap, &array);
+                    array.swap(index_pointer, min_index);
+                    index_pointer += *gap;
+                }
+            }
+        }
+    }
+
+    fn get_min<T: PartialOrd>(i: usize, gap: usize, array: &[T]) -> usize {
+
+        let mut min_index = i;
+        let mut current_min = &array[i];
+        let mut j = i + gap;
+
+        while j < array.len(){
+            if array[j] < *current_min {
+                current_min = &array[j];
+                min_index = j;
+            }
+            j += gap;
+        }
+        min_index
+    }
+}
+
+
--- rust-algo/sorting/src/main.rs
+++ rust-algo/sorting/src/main.rs
@@ -2,36 +2,56 @@ extern crate sorting;
 
 use sorting::selection;
 use sorting::insertion;
+use sorting::shell;
 use std::fmt::Debug;
 
-fn display_sort<T, F>(x: &[T], sort: F) 
-  where T: PartialOrd + Debug + Clone, 
-        F: Fn(&mut [T]) {
+fn display_sort<T, F>(x: &[T], sort: F)
+    where T: PartialOrd + Debug + Clone,
+          F: Fn(&mut [T]) {
 
-  println!("\nstart: {:?}", x);
+    println!("\nstart: {:?}", x);
 
-  let mut v = x.to_vec();
-  sort(&mut v);
+    let mut v = x.to_vec();
+    sort(&mut v);
 
-  println!("end: {:?} \n", v);
+    println!("end: {:?} \n", v);
 }
 
-
 fn main() {
-    
-    let v_float = vec![9.0, 8.0, 6.0, 7.0, 1.0, 2.0, 4.0, 3.0, 5.0];
-    let v_string = vec!["apple", "carrot", "banana", "deer", "d"];
-    let v_int = vec![1, 2, 3, 4, 5];
+
+    let v_sample = vec![62, 83, 18, 53, 7, 17, 95, 86, 47, 69, 25, 28];
+    let v_float: Vec<_> = (0..30).rev().map(|i| i as f64).collect();
+    let v_string = vec!["apple", "carrot", "banana", "deer", "d", "zebra"];
+    let v_int: Vec<usize> = (0..20).collect();
+
 
     println!("\n --------------- \n Selection Sort \n --------------- \n");
+    display_sort(&v_sample, selection::sort);
     display_sort(&v_float, selection::sort);
     display_sort(&v_string, selection::sort);
-    display_sort(&v_int, selection::sort);    
+    display_sort(&v_int, selection::sort);
 
     println!("\n --------------- \n Insertion Sort \n --------------- \n");
+    display_sort(&v_sample, insertion::sort);
     display_sort(&v_float, insertion::sort);
-    display_sort(&v_string, insertion::sort);    
-    display_sort(&v_int, insertion::sort);    
+    display_sort(&v_string, insertion::sort);
+    display_sort(&v_int, insertion::sort);
 
+    println!("\n --------------- \n Shell Sort \n --------------- \n");
+
+    let gaps = vec![5, 3, 1];
+    let mut array = v_sample.clone();
+
+    println!("\nstart: {:?}", array);
+    shell::sort(&mut array, &gaps);
+    println!("end: {:?} \n", array);
+
+
+    let gaps = vec![701, 301, 132, 57, 23, 10, 4, 1];
+    let mut array: Vec<_> = (0..30).rev().collect();
+
+    println!("\nstart: {:?}", array);
+    shell::sort(&mut array, &gaps);
+    println!("end: {:?} \n", array);
 }
 
