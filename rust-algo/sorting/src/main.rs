c018ed09d363a185faa2872234258fa8c0e71b12
Mithi Sevilla <mithi.sevilla@gmail.com>
2018-07-14 17:21:48 +0800

    Put tests to separate module

--- rust-algo/sorting/src/main.rs
+++ rust-algo/sorting/src/main.rs
@@ -1,76 +1,11 @@
 extern crate sorting;
-
-use sorting::selection;
-use sorting::insertion;
-use sorting::shell;
-use sorting::merge;
 use sorting::shuffle;
-use std::fmt::Debug;
-
-fn display_sort<T, F>(x: &[T], sort: F)
-    where T: PartialOrd + Debug + Clone,
-          F: Fn(&mut [T]) {
-
-    println!("\nstart: {:?}", x);
-
-    let mut v = x.to_vec();
-    sort(&mut v);
-
-    println!("end: {:?} \n", v);
-}
 
 fn main() {
-
-    let v_sample = vec![62, 83, 18, 53, 7, 17, 95, 86, 47, 69, 25, 28];
-    let v_float: Vec<_> = (0..30).rev().map(|i| i as f64).collect();
-    let v_string = vec!["apple", "carrot", "banana", "deer", "d", "zebra"];
-    let v_int: Vec<usize> = (0..20).collect();
-
-
-    println!("\n --------------- \n Selection Sort \n --------------- \n");
-    display_sort(&v_sample, selection::sort);
-    display_sort(&v_float, selection::sort);
-    display_sort(&v_string, selection::sort);
-    display_sort(&v_int, selection::sort);
-
-    println!("\n --------------- \n Insertion Sort \n --------------- \n");
-    display_sort(&v_sample, insertion::sort);
-    display_sort(&v_float, insertion::sort);
-    display_sort(&v_string, insertion::sort);
-    display_sort(&v_int, insertion::sort);
-
-    println!("\n --------------- \n Shell Sort \n --------------- \n");
-
-    let gaps = vec![5, 3, 1];
-    let mut array = v_sample.clone();
-
-    println!("\nstart: {:?}", array);
-    shell::sort(&mut array, &gaps);
-    println!("end: {:?} \n", array);
-
-
-    let gaps = vec![701, 301, 132, 57, 23, 10, 4, 1];
-    let mut array: Vec<_> = (0..30).rev().collect();
-
-    println!("\nstart: {:?}", array);
-    shell::sort(&mut array, &gaps);
-    println!("end: {:?} \n", array);
-
-    println!("\n --------------- \n Merge Sort \n --------------- \n");
-
-    let mut array: Vec<usize> = vec![8, 29, 15, 22, 23, 30, 16, 9, 11, 25, 14, 18, 28, 4, 7, 21, 23];
-    println!("\nstart: {:?}", array);
-    merge::sort(&mut array);
-    println!("end: {:?} \n", array);
 
     println!("\n --------------- \n Shuffle \n --------------- \n");
     let mut array: Vec<usize> = (0..30).collect();
 
-    println!("\nstart: {:?}", array);
-    shuffle(&mut array);
-    println!("end: {:?} \n", array);
-
-    let mut array = v_string.clone();
     println!("\nstart: {:?}", array);
     shuffle(&mut array);
     println!("end: {:?} \n", array);
---
+++ rust-algo/sorting/src/tests.rs
@@ -0,0 +1,67 @@
+use super::*;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::fmt::Debug;
+
+    pub fn check_sort<T, F>(x: &[T], sort: F)
+        where T: Ord + Clone + Debug,
+              F: Fn(&mut [T]) {
+
+        let mut v = x.to_vec();
+        shuffle(&mut v);
+        sort(&mut v);
+
+        assert_eq!(x.to_vec(), v);
+    }
+
+    pub fn check_shellsort<T>(x: &[T], gaps: &[usize])
+        where T: Ord + Clone + Debug {
+
+        let mut v = x.to_vec();
+        shuffle(&mut v);
+        shell::sort(&mut v, gaps);
+        assert_eq!(x.to_vec(), v);
+    }
+
+    #[test]
+    pub fn check_sorts() {
+
+        let v_int1 = vec![7, 17, 18, 25, 28, 47, 53, 62, 69, 83, 86, 95];
+        let v_int2 = vec![4, 7, 8, 9, 11, 14, 15, 16, 18, 21, 22, 23, 23, 25, 28, 29, 30];
+        let v_int3: Vec<usize> = (0..20).collect();
+        let v_string = vec!["apple", "banana", "carrot", "d", "deer", "zebra"] ;
+        let v_char = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        check_sort(&v_int1, selection::sort);
+        check_sort(&v_int2, selection::sort);
+        check_sort(&v_int3, selection::sort);
+        check_sort(&v_string, selection::sort);
+        check_sort(&v_char, selection::sort);
+
+        check_sort(&v_int1, insertion::sort);
+        check_sort(&v_int2, insertion::sort);
+        check_sort(&v_int3, insertion::sort);
+        check_sort(&v_string, insertion::sort);
+        check_sort(&v_char, insertion::sort);
+
+        check_sort(&v_int1, merge::sort);
+        check_sort(&v_int2, merge::sort);
+        check_sort(&v_int3, merge::sort);
+        check_sort(&v_string, merge::sort);
+        check_sort(&v_char, merge::sort);
+
+        let gaps = vec![701, 301, 132, 57, 23, 10, 4, 1];
+        check_shellsort(&v_int1, &gaps);
+        check_shellsort(&v_int2, &gaps);
+        check_shellsort(&v_int3, &gaps);
+        check_shellsort(&v_string, &gaps);
+        check_shellsort(&v_char, &gaps);
+
+    }
+}
+
+
