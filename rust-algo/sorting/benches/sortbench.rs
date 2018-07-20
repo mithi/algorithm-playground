985628181f1cdee418687d799ee477924ff3598b
Mithi Sevilla <mithi.sevilla@gmail.com>
2018-07-21 03:10:38 +0800

    FIX bug in benches

--- rust-algo/sorting/benches/sortbench.rs
+++ rust-algo/sorting/benches/sortbench.rs
@@ -20,185 +20,157 @@ lazy_static! {
         array
     };
 
-    static ref NUMBERS: Vec<usize> = largedata::random_numbers(3000, 500);
+    static ref NUMBERS: Vec<usize> = largedata::random_numbers(3000, 100);
     static ref STRINGS: Vec<String> = largedata::random_strings(3000);
     static ref GAPS: Vec<usize> = vec![701, 301, 132, 57, 23, 10, 4, 1];
 }
 
 //----------------------------------------------------------------------------
 
-fn numbers_merge(c: &mut Criterion) {
-    c.bench_function("> NUMBERS: Merge Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| merge::sort(&mut d))
-    });
-}
+fn numbers_bench(c: &mut Criterion) {
 
-fn numbers_merge_bottomsup(c: &mut Criterion) {
-    c.bench_function("> NUMBERS: Merge Bottomsup Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| merge::bottomsup_sort(&mut d))
-    });
-}
-
-fn numbers_quick(c: &mut Criterion) {
-    c.bench_function("> NUMBERS: Quick Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| quick::sort(&mut d))
-    });
-}
-
-fn numbers_improved_quick(c: &mut Criterion) {
-    c.bench_function("> NUMBERS: Improved Quick Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| quick::improved_sort(&mut d))
-    });
-}
-
-fn numbers_insertion(c: &mut Criterion) {
-    c.bench_function("> NUMBERS: Insertion Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| insertion::sort(&mut d))
-    });
-}
-
-fn numbers_selection(c: &mut Criterion) {
     c.bench_function("> NUMBERS: Selection Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| selection::sort(&mut d))
+        b.iter(|| selection::sort(&mut NUMBERS.clone()))
     });
-}
 
-fn numbers_shell(c: &mut Criterion) {
+    c.bench_function("> NUMBERS: Insertion Sort", move |b| {
+        b.iter(|| insertion::sort(&mut NUMBERS.clone()))
+    });
+
     c.bench_function("> NUMBERS: Shell Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| shell::sort(&mut d, &GAPS))
+        b.iter(|| shell::sort(&mut NUMBERS.clone(), &GAPS))
+    });
+
+    c.bench_function("> NUMBERS: Merge Sort", move |b| {
+        b.iter(|| merge::sort(&mut NUMBERS.clone()))
+    });
+
+    c.bench_function("> NUMBERS: Merge Bottomsup Sort", move |b| {
+        b.iter(|| merge::bottomsup_sort(&mut NUMBERS.clone()))
+    });
+
+    c.bench_function("> NUMBERS: Quick Sort", move |b| {
+        b.iter(|| quick::sort(&mut NUMBERS.clone()))
+    });
+
+    c.bench_function("> NUMBERS: Improved Quick Sort", move |b| {
+        b.iter(|| quick::improved_sort(&mut NUMBERS.clone()))
     });
 }
+
+
 
 //----------------------------------------------------------------------------
 
-fn unique_numbers_merge(c: &mut Criterion) {
-    c.bench_function("> UNIQUE NUMBERS: Merge Sort", move |b| {
-        let mut d = UNIQUE_NUMBERS.to_vec();
-        b.iter(|| merge::sort(&mut d))
-    });
-}
+fn unique_numbers_bench(c: &mut Criterion) {
 
-fn unique_numbers_merge_bottomsup(c: &mut Criterion) {
-    c.bench_function("> UNIQUE NUMBERS: Merge Bottomsup Sort", move |b| {
-        let mut d = UNIQUE_NUMBERS.to_vec();
-        b.iter(|| merge::bottomsup_sort(&mut d))
-    });
-}
-
-fn unique_numbers_quick(c: &mut Criterion) {
-    c.bench_function("> UNIQUE NUMBERS: Quick Sort", move |b| {
-        let mut d = UNIQUE_NUMBERS.to_vec();
-        b.iter(|| quick::sort(&mut d))
-    });
-}
-
-fn unique_numbers_improved_quick(c: &mut Criterion) {
-    c.bench_function("> UNIQUE NUMBERS: Improved Quick Sort", move |b| {
-        let mut d = UNIQUE_NUMBERS.to_vec();
-        b.iter(|| quick::improved_sort(&mut d))
-    });
-}
-
-fn unique_numbers_insertion(c: &mut Criterion) {
-    c.bench_function("> UNIQUE NUMBERS: Insertion Sort", move |b| {
-        let mut d = UNIQUE_NUMBERS.to_vec();
-        b.iter(|| insertion::sort(&mut d))
-    });
-}
-
-fn unique_numbers_selection(c: &mut Criterion) {
     c.bench_function("> UNIQUE NUMBERS: Selection Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| selection::sort(&mut d))
+        b.iter(|| selection::sort(&mut UNIQUE_NUMBERS.clone()))
     });
-}
 
-fn unique_numbers_shell(c: &mut Criterion) {
+    c.bench_function("> UNIQUE NUMBERS: Insertion Sort", move |b| {
+        b.iter(|| insertion::sort(&mut UNIQUE_NUMBERS.clone()))
+    });
+
     c.bench_function("> UNIQUE NUMBERS: Shell Sort", move |b| {
-        let mut d = NUMBERS.to_vec();
-        b.iter(|| shell::sort(&mut d, &GAPS))
+        b.iter(|| shell::sort(&mut UNIQUE_NUMBERS.clone(), &GAPS))
+    });
+
+    c.bench_function("> UNIQUE NUMBERS: Merge Sort", move |b| {
+        b.iter(|| merge::sort(&mut UNIQUE_NUMBERS.clone()))
+    });
+
+    c.bench_function("> UNIQUE NUMBERS: Merge Bottomsup Sort", move |b| {
+        b.iter(|| merge::bottomsup_sort(&mut UNIQUE_NUMBERS.clone()))
+    });
+
+    c.bench_function("> UNIQUE NUMBERS: Quick Sort", move |b| {
+        b.iter(|| quick::sort_recurse(&mut UNIQUE_NUMBERS.clone()))
+    });
+
+    c.bench_function("> UNIQUE NUMBERS: Improved Quick Sort", move |b| {
+        b.iter(|| quick::improved_sort(&mut UNIQUE_NUMBERS.clone()))
     });
 }
 
 //----------------------------------------------------------------------------
 
-fn strings_merge(c: &mut Criterion) {
-    c.bench_function("> STRINGS: Merge Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| merge::sort(&mut d))
-    });
-}
+fn strings_bench(c: &mut Criterion) {
 
-fn strings_merge_bottomsup(c: &mut Criterion) {
-    c.bench_function("> STRINGS: Merge Bottomsup Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| merge::bottomsup_sort(&mut d))
-    });
-}
-
-fn strings_quick(c: &mut Criterion) {
-    c.bench_function("> STRINGS: Quick Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| quick::sort(&mut d))
-    });
-}
-
-fn strings_improved_quick(c: &mut Criterion) {
-    c.bench_function("> STRINGS: Improved Quick Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| quick::improved_sort(&mut d))
-    });
-}
-
-fn strings_insertion(c: &mut Criterion) {
-    c.bench_function("> STRINGS: Insertion Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| insertion::sort(&mut d))
-    });
-}
-
-fn strings_selection(c: &mut Criterion) {
     c.bench_function("> STRINGS: Selection Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| selection::sort(&mut d))
+        b.iter(|| selection::sort(&mut STRINGS.clone()))
+    });
+
+    c.bench_function("> STRINGS: Insertion Sort", move |b| {
+        b.iter(|| insertion::sort(&mut STRINGS.clone()))
+    });
+
+    c.bench_function("> STRINGS: Shell Sort", move |b| {
+        b.iter(|| shell::sort(&mut STRINGS.clone(), &GAPS))
+    });
+
+    c.bench_function("> STRINGS: Merge Sort", move |b| {
+        b.iter(|| merge::sort(&mut STRINGS.clone()))
+    });
+
+    c.bench_function("> STRINGS: Merge Bottomsup Sort", move |b| {
+        b.iter(|| merge::bottomsup_sort(&mut STRINGS.clone()))
+    });
+
+
+    c.bench_function("> STRINGS: Quick Sort", move |b| {
+        b.iter(|| quick::sort(&mut STRINGS.clone()))
+    });
+
+    c.bench_function("> STRINGS: Improved Quick Sort", move |b| {
+        b.iter(|| quick::improved_sort(&mut STRINGS.clone()))
     });
 }
 
-fn strings_shell(c: &mut Criterion) {
-    c.bench_function("> STRINGS: Shell Sort", move |b| {
-        let mut d = STRINGS.to_vec();
-        b.iter(|| shell::sort(&mut d, &GAPS))
+
+fn shuffle_bench(c: &mut Criterion) {
+
+    c.bench_function("> UNIQUE_NUMBERS: Shuffle 1", move |b| {
+        b.iter(|| shuffle(&mut UNIQUE_NUMBERS.clone()))
+    });
+
+    c.bench_function("> NUMBERS: Shuffle 1", move |b| {
+        b.iter(|| shuffle(&mut NUMBERS.clone()))
+    });
+
+    c.bench_function("> STRINGS: Shuffle 1", move |b| {
+        b.iter(|| shuffle(&mut STRINGS.clone()))
+    });
+
+    c.bench_function("> UNIQUE_NUMBERS: Shuffle 2", move |b| {
+        b.iter(|| thread_rng().shuffle(&mut UNIQUE_NUMBERS.clone()))
+    });
+
+    c.bench_function("> NUMBERS: Shuffle 2", move |b| {
+        b.iter(|| thread_rng().shuffle(&mut NUMBERS.clone()))
+    });
+
+    c.bench_function("> STRINGS: Shuffle 2", move |b| {
+        b.iter(|| thread_rng().shuffle(&mut STRINGS.clone()))
+    });
+}
+
+fn clone_bench(c: &mut Criterion) {
+
+    c.bench_function("> UNIQUE_NUMBERS: clone", move |b| {
+        b.iter(|| UNIQUE_NUMBERS.clone())
+    });
+
+    c.bench_function("> NUMBERS: clone", move |b| {
+        b.iter(|| NUMBERS.clone())
+    });
+
+    c.bench_function("> STRINGS: clone", move |b| {
+        b.iter(|| STRINGS.clone())
     });
 }
 
 //----------------------------------------------------------------------------
 
-criterion_group!(numbers_benches,
-    numbers_merge_bottomsup, numbers_merge,
-    numbers_quick, numbers_improved_quick,
-    numbers_selection, numbers_insertion,
-    numbers_shell,
-);
-
-criterion_group!(unique_numbers_benches,
-    unique_numbers_merge_bottomsup, unique_numbers_merge,
-    unique_numbers_quick, unique_numbers_improved_quick,
-    unique_numbers_selection, unique_numbers_insertion,
-    unique_numbers_shell,
-);
-
-criterion_group!(strings_benches,
-    strings_merge_bottomsup, strings_merge,
-    strings_quick, strings_improved_quick,
-    strings_selection, strings_insertion,
-    strings_shell,
-);
-
-criterion_main!(numbers_benches, unique_numbers_benches, strings_benches);
+criterion_group!(benches, numbers_bench, unique_numbers_bench, strings_bench, shuffle_bench, clone_bench);
+criterion_main!(benches);
