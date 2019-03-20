351d2d38c6fcd7497c3ab1baabb8c350347e6e55
Mithi Sevilla <mithi.sevilla@gmail.com>
2019-03-20 15:38:22 +0800

    Implement base64 converter with error handling

---
+++ rust-examples/code007.rs
@@ -0,0 +1,112 @@
+// convert string in base 64 to vector of u8
+// https://docs.rs/crate/base64/0.3.1/source/tests/tests.rs
+#[macro_use]
+extern crate lazy_static;
+use std::collections::HashMap;
+
+const ALL64: [char; 64] = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+    ];
+
+
+lazy_static! {
+    static ref MAP64: HashMap<char, usize> = {
+        let mut m = HashMap::new();
+        for i in 0..ALL64.len() {
+            m.insert(ALL64[i], i);
+        }
+        m
+    };
+}
+
+
+fn push_three(v: &mut Vec<u8>, r: usize) {
+    let mask = 255;
+    let (a, b, c) = ((r >> 16) & mask, (r >> 8) & mask, r & mask);
+    v.push(a as u8);
+    v.push(b as u8);
+    v.push(c as u8);
+}
+
+
+fn get64(x: &Option<char>) -> Result<usize, &'static str>{
+    assert!(*x != None);
+    let c = x.unwrap();
+    if let Some(&r) = MAP64.get(&c) {
+        Ok(r)
+    } else {
+        Err("Invalid character")
+    }
+}
+
+
+fn from_base64(s: &str) -> Result<Vec<u8>, &'static str> {
+    if s.len() % 4 == 1 { return Err("Invalid length") }
+    let mask = 255;
+
+    let mut v = Vec::new();
+    let l = (s.len() as f32 / 4.0).ceil() as usize - 1;
+    let mut chars = s.chars();
+
+    for _ in 0..l {
+        let mut r = 0;
+        for _ in 0..4 {
+            r = (r << 6) + get64(&chars.next())?;
+        }
+        push_three(&mut v, r);
+    }
+
+    let a = get64(&chars.next())?;
+    let b = get64(&chars.next())?;
+
+    let mut r = (a  << 18) + (b << 12);
+
+    let a = (chars).next().unwrap_or('=');
+    let b = (chars).next().unwrap_or('=');
+
+    if a != '=' {
+        let a = MAP64.get(&a).ok_or("Invalid character")?;
+        r = r + (a << 6);
+
+        if b != '=' {
+            let b = MAP64.get(&b).ok_or("Invalid character")?;
+            r = r + b;
+            push_three(&mut v, r);
+        } else {
+            v.push((r >> 16 & mask) as u8);
+            v.push((r >> 8 & mask) as u8);
+        }
+
+    } else {
+        if b != '=' { return Err("Invalid length") };
+        v.push((r >> 16) as u8);
+    }
+    Ok(v)
+}
+
+
+fn main() -> Result<(), &'static str> {
+
+    let s = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    let t = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+    let v = from_base64(&s)?;
+    let w: Vec<u8> = vec![
+        73, 39, 109, 32, 107, 105, 108, 108, 105, 110, 103, 32, 121, 111, 117, 114,
+        32, 98, 114, 97, 105, 110, 32, 108, 105, 107, 101, 32, 97, 32, 112, 111, 105,
+        115, 111, 110, 111, 117, 115, 32, 109, 117, 115, 104, 114, 111, 111, 109
+        ];
+
+    assert_eq!(v, w);
+    println!("{}", String::from_utf8(from_base64("Zg==")?).unwrap()); // f
+    println!("{}", String::from_utf8(from_base64("VGVzdAo=")?).unwrap()); // Test\n
+    println!("{}", String::from_utf8(from_base64(&t)?).unwrap());
+    println!("{}", String::from_utf8(from_base64("YW55IGNhcm5hbCBwbGVhc3VyZS4=")?).unwrap()); //any carnal pleasure.
+    println!("{}", String::from_utf8(from_base64("YW55IGNhcm5hbCBwbGVhc3VyZQ==")?).unwrap()); //any carnal pleasure
+    println!("{}", String::from_utf8(from_base64("YW55IGNhcm5hbCBwbGVhcw==")?).unwrap()); //any carnal pleas
+
+    Ok(())
+}
