6eb6665af51b0ac266a73da045bbd2b4c08aea74
Mithi <mithi.sevilla@gmail.com>
2020-02-12 18:56:57 +0800

    Add interactive input fields to change measurements

--- simple-exercises/dash-exercises/app-8.py
+++ simple-exercises/dash-exercises/app-8.py
@@ -1,12 +1,21 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 print(dcc.__version__) # 0.6.0 or above is required
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# Since we're adding callbacks to elements that don't exist in the app.layout,
+# Dash will raise an exception to warn us that we might be
+# doing something wrong.
+# In this case, we're adding the elements through a callback, so we can ignore
+# the exception.
+app.config.suppress_callback_exceptions = True
+
 
 app.layout = html.Div([
   # represents the URL bar, doesn't render anything
@@ -28,21 +37,39 @@ app.layout = html.Div([
 ])
 
 # ----
+# INPUTS
+# ----
+
+input_measure_front = dcc.Input(
+  id='input-measure-front',
+  type='number',
+  value=15
+)
+
+input_measure_side = dcc.Input(
+  id='input-measure-side',
+  type='number',
+  value=25
+)
+
+input_measure_mid = dcc.Input(
+  id='input-measure-mid',
+  type='number',
+  value=50
+)
+# ----
 # PAGES
 # ----
+
+# Measurements 
+div_hexapod_measurements = html.Div(id='div-hexapod-measurements')
+
 page_measurements = html.Div([
-  html.H1('Measurements of this Hexapod'),
-  html.Div(dcc.Markdown('''
-    ### Body Measurements
-      - Front: **44** mm
-      - Side: **33** mm
-      - middle: **100** mm
-    ### Leg measurements
-      - coxia: **89** mm
-      - femur: **72** mm
-      - tibia: ** 300** mm
-    '''
-  ))
+  div_hexapod_measurements,
+  html.H1('Please Input measurements of this Hexapod'),
+  html.H4(["Front: ", input_measure_front]),
+  html.H4(["Middle: ", input_measure_mid]),
+  html.H4(["Side: ", input_measure_side]),
 ])
 
 page_kinematics = html.Div([
@@ -66,12 +93,43 @@ PAGES = {
   '/': page_root
 }
 
+
 @app.callback(
-  dash.dependencies.Output('page-content', 'children'),
-  [dash.dependencies.Input('url', 'pathname')]
+  Output('div-hexapod-measurements', 'children'),
+  [
+    Input('input-measure-front', 'value'),
+    Input('input-measure-side', 'value'),
+    Input('input-measure-mid', 'value')
+  
+  ]
+)
+def display_measurements(front, side, mid):
+  if front is None:
+    raise dash.exceptions.PreventUpdate
+  if side is None:
+    raise dash.exceptions.PreventUpdate
+  if mid is None:
+    raise dash.exceptions.PreventUpdate
+
+  front = max(0, front)
+  side = max(0, side)
+  mid = max(0, mid)
+
+  return dcc.Markdown('''
+    ### front: {}
+    ### side: {}
+    ### middle: {}
+    '''.format(front, side, mid))
+
+@app.callback(
+  Output('page-content', 'children'),
+  [Input('url', 'pathname')]
 )
 def display_page(pathname):
-  return PAGES[pathname]
+  try:
+    return PAGES[pathname]
+  except KeyError:
+    return html.Div([dcc.Markdown('# 404')])
 
 if __name__ == '__main__':
   app.run_server(debug=True)
